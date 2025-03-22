6e8f86e5e39b2eb04ad9a1d7eab7ebab10754d5a
akulkarthikeya006@gmail.com <akulkarthikeya006@gmail.com>
2025-03-22 11:28:07 +0530

    asap

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get("/ping",(req,res)=>{
-    resizeBy.send("pong!");
-})
 
-app.listen(8080,()=>{
+app.get('/ping', (req, res) => {
     try {
-        console.log("Server connected sucessfully");
+        res.status(200).send({msg:'pong'});
     } catch (error) {
-        console.log(error)
+        res.status(500).send({msg:"something went wrong"})
     }
-})
+});
+app.get('/', (req, res) => {
+    res.send('Connected to mongodb successfully');
+  });
+
+
+app.listen(3000, async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Server connected successfully!")
+    } catch (error) {
+        console.log("Error")
+    }
+});
